a9fb6e09995f8c95cb9f6a7632f265990a70c91a
Kauan Garcia <163343832+KG4rcia@users.noreply.github.com>
2025-11-13 14:00:24 -0300

    Atualização de tarefas.py
    Foi adicionado novas funções e correção de alguns bugs que tinha dentro do código.

--- tarefas.py
+++ tarefas.py
@@ -12,11 +12,87 @@ mapa_prioridade = {
     "Baixa": 3
 }
 
+apagadas = 0
+finalizadas = 0
+pendentes = 0
+
 os.system('cls')
 tarefas = []
+historico = []
 tarefas_arquivadas = []
 agora = datetime.now()
 formato_esperado = "%d/%m/%Y"
+
+def ver_historico():
+    '''
+    A função ver historico itera sobre o lista de dicionários historico com o método de listas enumerate()
+    '''
+    print(f"\n {Fore.YELLOW}=== Entrando na função de Ver Historico [📒] === \n")
+    print(f"\n{Fore.RED}======================\n")
+
+    if not historico:
+        print(f"{Fore.RED}--ERRO: Lista está vazia [❗]")
+        print(f"\n{Fore.RED}======================\n")
+        return
+    
+    for salvos in historico:
+        for nome, conteudo in salvos.items():
+            print(f"{Fore.YELLOW}{nome} | {conteudo}")
+    print("\n======================\n")
+
+def salvar_historico_json():
+    '''
+    Essa função vai fazer um arquivo json que vai salvar todo o histórico de alteração das tarefas
+    '''
+    try:
+        with open("historico.json", "w", encoding='utf-8') as f:
+            json.dump(historico, f, indent=4, ensure_ascii=False)
+    except Exception as ex:
+        print(f"\n-- {Fore.RED}ERRO: Ocorreu um erro em salvar {Style.BRIGHT}JSON historico{Style.RESET_ALL}{Fore.RED}: {ex} [❗]\n")
+
+def carregar_json_historico():
+    '''
+    carregando arquivo json historico dentro de um try e com with open para mais segurança
+    '''
+    try:
+        if os.path.exists("historico.json"):
+            with open("historico.json", "r", encoding='utf-8') as f:
+                dados = json.load(f)
+                if not dados:
+                    print(f"\n {Fore.RED} === Nenhum arquivo {Style.BRIGHT}JSON historico{Style.RESET_ALL}{Fore.RED} encontrado. Vamos iniciar com uma lista vazia === \n")                    
+                    return []
+                return dados
+        else:
+            print(f"\n {Fore.RED} === Nenhum arquivo {Style.BRIGHT}JSON historico{Style.RESET_ALL}{Fore.RED} encontrado. Vamos iniciar com uma lista vazia === \n")
+            return []
+    except Exception as ex:
+        print(f"\n-- {Fore.RED}ERRO: Ocorreu um erro inesperado em carregar {Style.BRIGHT}JSON historico{Style.RESET_ALL}{Fore.RED}: {ex} [❗]\n")
+
+def apagar_tarefa(ind_tarefa):
+    '''
+    A função apagar tarefa não apaga literalmente da nossa lista de dicionários, mas muda o status dela para "Deletada". 
+    Isso faz com que mesmo que a prioridade original dela seja de urgente ou qualquer outra, ela nunca seja concluída. 
+    '''
+
+    while True:
+        confirmar_se_apagar = input(f"{Fore.YELLOW}-> Você tem certeza que deseja apagar essa tarefa? {Style.BRIGHT}\nSe ela for apagada, você não vai conseguir recuperar ela:{Style.RESET_ALL} ").title()
+        if confirmar_se_apagar == "Sim":
+            print(f"\n {Fore.YELLOW}=== Entrando na função de Apagar Tarefa [📒] === \n")
+            tarefas[ind_tarefa]["Status"] = "Deletada"
+            tarefas[ind_tarefa]["Prioridade"] = "N/A"
+            tarefas[ind_tarefa]["Conclusão"] = "N/A"
+            agora = datetime.now()
+            formatando = agora.strftime("%d/%m/%Y - %H:%M")
+            apagada_historico = {tarefas[ind_tarefa]['Nome da tarefa']:f"Apagada em {formatando}"}
+            historico.append(apagada_historico)
+            salvar_historico_json()
+            break
+            print(f"\n{Fore.GREEN}=== A tarefa {tarefas[ind_tarefa]["Nome da tarefa"]} foi deletada [🗑️] ===\n")
+        elif confirmar_se_apagar == "Não":
+            print(f"\n{Fore.BLUE}=== Saindo... === \n")
+            break
+        else:
+            print(f"\n{Fore.RED}--ERRO: Escolha uma opção válida. {Style.BRIGHT}Ela deve ser 'Sim' ou 'Não'{Style.RESET_ALL} {Fore.RED}[❗]\n")
 
 def carregar_json_arquivada():
     '''
@@ -101,9 +177,14 @@ def criar_tarefa(nome_tarefa, descricao_tarefa, prioridade_tarefa, origem_tarefa
     '''
     criando uma nova tafera com os parâmetros que a função recebe e adicionando a lista de tarefas com o método append
     '''
-    print(F"\n {Fore.BLUE}=== Entrando na função 'Criar Tarefa' ===\n")
+    print(f"\n {Fore.YELLOW}=== Entrando na função de Criar Tarefa [📒] === \n")
     tarefa = {"Nome da tarefa": nome_tarefa, "Descrição": descricao_tarefa, "Prioridade": prioridade_tarefa, "Origem": origem_tarefa, "Status": "Pendente", "Conclusão": "Não Definido"}
     tarefas.append(tarefa)
+    agora = datetime.now()
+    formatando = agora.strftime("%d/%m/%Y - %H:%M")
+    criada_historico = {nome_tarefa:f"Criada em {formatando}"}
+    historico.append(criada_historico)
+    salvar_historico_json()
     print(f"\n{Fore.BLUE}=== Tarefa Adicionada, função concluída com sucesso. === \n")
     organizar_lista()
     return
@@ -113,25 +194,38 @@ def procurar_tarefa():
     usano um input dentro de um try para pedir o índice da tarefa que o usuário quer, e depois usando um for dentro da lista com a função enumarete
     para pegar o índice e o valor do item, se o índice for o mesmo, ele retorna a tarefa, caso não, ele não retorna nada além de um print
     '''
+    print(f"\n {Fore.YELLOW}=== Entrando na função de Procurar Tarefa [📒] === \n")
+    while True:
+        relatorio()
 
-    print(f'''
-======================
-          
+        print(f'''
 Você entrou na função {Fore.GREEN}"Procurar Tarefa"{Style.RESET_ALL}
 
-{Fore.YELLOW}Digite o número da tarefa que você deseja atualizar.{Style.RESET_ALL}
+{Fore.YELLOW}Digite o número da tarefa que você está procurando.{Style.RESET_ALL}
+
+{Fore.BLACK}{Style.BRIGHT}Digite "Sair" para cancelar.
           
 ======================''')
-    relatorio()
-    try: 
-        indice_tarefa = int(input("Digite o número da tarefa que você está escolhendo: "))
-    except Exception as e:
-        print(f"\n-- {Fore.RED}ERRO: Ocorreu um erro inesperado: {e} [❗]\n")
+        
+        indice_tarefa = input(f"{Fore.YELLOW}Digite o número da tarefa que você quer{Style.RESET_ALL}: ").title()
+
+        if indice_tarefa == "Sair":
+            break
+
+        if indice_tarefa.isdigit():
+            indice_tarefa_convertido = int(indice_tarefa)
+            break
+        else:
+            print(f"\n{Fore.RED}-- ERRO: A função espera somente {Style.BRIGHT}números{Style.RESET_ALL} {Fore.RED}ou {Style.BRIGHT}'sair'{Style.RESET_ALL}. {Fore.RED}Por favor, digite uma informação válida. \n")
+
+    if indice_tarefa == "Sair":
+        print(f"\n{Fore.BLUE}=== Saindo... === \n")
+        return None
 
     for indice, tarefa in enumerate(tarefas):
-        if indice == indice_tarefa:
+        if indice == indice_tarefa_convertido:
             print(f"\n {Fore.GREEN}=== Tarefa Encontrada === \n")
-            return indice_tarefa
+            return indice_tarefa_convertido
     else:
         print(f"\n{Fore.RED}=== Tarefa com índice {indice_tarefa} não existe [❗] ===")
 
@@ -185,6 +279,11 @@ A data da atividade deve ser informada desse jeito: {agora.day}/{agora.month}/{a
                     if ver_se_arquivar:
                         print(f"\n {Fore.GREEN}=== Tarefa {tarefa} arquivada === \n")
                         tarefas_arquivadas.append(tarefa)
+                        agora = datetime.now()
+                        formatando = agora.strftime("%d/%m/%Y - %H:%M")
+                        arquivada_historico = {tarefa['Nome da tarefa']:f"Arquivada em {formatando}"}
+                        historico.append(arquivada_historico)
+                        salvar_historico_json()
                     break
                     
                 except Exception as ex:
@@ -245,6 +344,20 @@ O que você deseja alterar?:
         tarefas[ind_tarefa]["Prioridade"] = nova_prioridade
         print(f"\n {Fore.GREEN} === Prioridade atualizado com sucesso === \n")
     organizar_lista()
+    agora = datetime.now()
+    formatando = agora.strftime("%d/%m/%Y - %H:%M")
+    if alterar == "1":
+        alterada_historico = {tarefas[ind_tarefa]['Nome da tarefa']:f"Teve o nome alterado em {formatando}"}
+        historico.append(alterada_historico)
+        salvar_historico_json()
+    elif alterar == "2":
+        alterada_historico = {tarefas[ind_tarefa]['Nome da tarefa']:f"Teve a descrição alterada em {formatando}"}
+        historico.append(alterada_historico)
+        salvar_historico_json()
+    elif alterar == "3":
+        alterada_historico = {tarefas[ind_tarefa]['Nome da tarefa']:f"Teve a prioridade alterada em {formatando}"}
+        historico.append(alterada_historico)
+        salvar_historico_json()
 
 def verificar_urgencia():
     print(f"\n{Fore.YELLOW}=== Entrando na função de Verificar Urgencia [📒] === \n")
@@ -304,8 +417,27 @@ def relatorio():
         return
 
     print("======================")
+    
+    global apagadas
+    global finalizadas
+    global pendentes
+
     for indice, tarefa in enumerate(tarefas):
-        print(f"N° da tarefa: {indice} | Tarefa: {tarefa['Nome da tarefa']} - Descrição: {tarefa['Descrição']} - Origem: {tarefa['Origem']} - Status: {tarefa['Status']}")
+        if tarefa['Status'] == "Deletada":
+            print(f"N° da tarefa: {Fore.RED}{indice}{Style.RESET_ALL} | Tarefa: {Fore.RED}{tarefa['Nome da tarefa']}{Style.RESET_ALL} - Descrição: {Fore.RED}{tarefa['Descrição']}{Style.RESET_ALL} - Origem: {Fore.RED}{tarefa['Origem']}{Style.RESET_ALL} - Status: {Fore.RED}{tarefa['Status']}{Style.RESET_ALL} - Prioridade: {Fore.RED}{tarefa['Prioridade']}{Style.RESET_ALL}")
+            apagadas += 1
+        elif tarefa['Status'] == "Concluido":
+            print(f"N° da tarefa: {Fore.GREEN}{indice}{Style.RESET_ALL} | Tarefa: {Fore.GREEN}{tarefa['Nome da tarefa']}{Style.RESET_ALL} - Descrição: {Fore.GREEN}{tarefa['Descrição']}{Style.RESET_ALL} - Origem: {Fore.GREEN}{tarefa['Origem']}{Style.RESET_ALL} - Status: {Fore.GREEN}{tarefa['Status']}{Style.RESET_ALL} - Prioridade: {Fore.GREEN}{tarefa['Prioridade']}{Style.RESET_ALL}")            
+            finalizadas += 1
+        else:
+            print(f"N° da tarefa: {Fore.YELLOW}{indice}{Style.RESET_ALL} | Tarefa: {Fore.YELLOW}{tarefa['Nome da tarefa']}{Style.RESET_ALL} - Descrição: {Fore.YELLOW}{tarefa['Descrição']}{Style.RESET_ALL} - Origem: {Fore.YELLOW}{tarefa['Origem']}{Style.RESET_ALL} - Status: {Fore.YELLOW}{tarefa['Status']}{Style.RESET_ALL} - Prioridade: {Fore.YELLOW}{tarefa['Prioridade']}{Style.RESET_ALL}")
+            pendentes +=1
+
+    print(f'''
+✅{Fore.GREEN} -> Há {finalizadas} concluidas.{Style.RESET_ALL} 
+❌{Fore.RED} -> Há {apagadas} apagadas.{Style.RESET_ALL} 
+🟨{Fore.YELLOW} -> Há {pendentes} pendentes.{Style.RESET_ALL} 
+''')    
     print("======================")
 
 def ver_arquivadas():
@@ -340,20 +472,21 @@ while True:
 5. Excluir tarefa.
 6. Relatório.
 7. Relatórios Arquivados.
-8. Sair.
+8. Histórico de mudanças.
+9. Sair.
 {Style.RESET_ALL}
                             
 - > {Fore.GREEN}Sua escolha: {Style.RESET_ALL}''').title()
         if escolher_tarefa == "1":
             nome_tarefa = input("- > Digite o nome da tarefa: ").title()
-            descricao = input("- > De uma descrição da tarefa: ").title()
+            descricao = input("- > De uma descrição da tarefa: ")
             while True:
                 prioridade = input("- > Qual a prioridade?[Urgente/Alta/Média/Baixa]: ").title()
                 if prioridade not in prioridades:
                     print(f"\n{Fore.RED} === Escolha uma prioridade váida [❗]===\n")
                 else:
                     break
-            origem = input("- > Qual a origem da tarefa?: ").title()
+            origem = input("- > Qual a origem da tarefa?: ")
             criar_tarefa(nome_tarefa, descricao, prioridade, origem)
         
         elif escolher_tarefa == "2":
@@ -361,21 +494,29 @@ while True:
         
         elif escolher_tarefa == "3":
             id = procurar_tarefa()
-            alterar_tarefa(id)
+            if id == None:
+                continue
+            else:
+                alterar_tarefa(id)
         
         elif escolher_tarefa == "4":
             concluir_tarefa()
         
         elif escolher_tarefa == "5":
-            ...
-        
+            id = procurar_tarefa()
+            apagar_tarefa(id)
+
         elif escolher_tarefa == "6":
             relatorio()
 
         elif escolher_tarefa == "7":
             ver_arquivadas()
-        
+
         elif escolher_tarefa == "8":
+            ver_historico()
+            
+
+        elif escolher_tarefa == "9":
             if not tarefas and not tarefas_arquivadas:
                 print(f"{Fore.RED}=== Finalizando ===")
                 break
@@ -398,5 +539,5 @@ while True:
                 print(i )
         else:
             print(f"\n{Fore.RED}=== Escolha uma opção válida === \n")
-    except Exception as e:
+    except Exception as e: 
         print(f"\n-- {Fore.RED}ERRO: Ocorreu um erro inesperado: {e} [❗]\n")
